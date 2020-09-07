fe9ae35f1c88e9adff3156b5e135f54541942437
Balazs Juhasz <juhaszbal@gmail.com>
2020-09-07 17:01:17 +0200

    Updates

---
+++ precache-manifest.3a39d345bf3f128dfeeba4acf47a2839.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e34a61c729b9ff14ab6fdbcf51129c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "671939116ed8cbdb9e36",
+    "url": "/monsters-rolodex/static/css/main.ab621c26.chunk.css"
+  },
+  {
+    "revision": "59cc62ea009f8867b4b7",
+    "url": "/monsters-rolodex/static/js/2.5925a088.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.5925a088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "671939116ed8cbdb9e36",
+    "url": "/monsters-rolodex/static/js/main.53a06a96.chunk.js"
+  },
+  {
+    "revision": "6493a01b90e2fa8006fd",
+    "url": "/monsters-rolodex/static/js/runtime-main.c8f0c5b4.js"
+  }
+]);
